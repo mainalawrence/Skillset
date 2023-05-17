1e0a0cb7297e5ff076290433115fded1cdaf9f91
maina lawrence <mainalawrence32@gmail.com>
2023-05-17 17:00:57 +0300

    chat with socket io

---
+++ Server/build/dist/server.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const socket_io_1 = require("socket.io");
+const http_1 = __importDefault(require("http"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    origin: "http://localhost:3000"
+}));
+const server = http_1.default.createServer(app);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+const io = new socket_io_1.Server(server);
+io.on('connection', (socket) => {
+    socket.broadcast.emit('Hello');
+    console.log("Connected");
+    socket.on('disconnect', () => {
+        console.log(`user disconnected`);
+    });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+    socket.on('foo', (msg) => {
+        io.emit('foo', msg);
+    });
+});
+server.listen(4000, () => {
+    console.log("Server Started");
+});
---
+++ Server/server.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import { Server, Socket } from "socket.io";
+import http from 'http';
+import cors from 'cors';
+
+const app = express();
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+const io = new Server(server);
+
+io.on('connection', (socket: any) => {
+    socket.broadcast.emit('Hello');
+    console.log("Connected");
+    socket.on('disconnect', () => {
+        console.log(`user disconnected`);
+    });
+    socket.on('chat message', (msg: String) => {
+        io.emit('chat message', msg);
+    });
+    socket.on('foo', (msg: String) => {
+        io.emit('foo', msg);
+    })
+
+})
+
+
+server.listen(4000, () => {
+    console.log("Server Started");
+})
